c7036e357018ee9066026a385215584be70d80f2
Rafael Casachi <rcasachi@gmail.com>
2018-09-25 09:06:33 -0300

    Roles views, controller and routes

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -8,7 +8,13 @@
     <p class="menu-label">Administration</p>
     <ul class="menu-list">
       <li><a href="{{ route('users.index') }}">Manage Users</a></li>
-      <li><a href="{{route('permissions.index')}}">Roles &amp; Permissions</a></li>
+      <li>
+        <a href="{{route('permissions.index')}}">Roles &amp; Permissions</a>
+        <ul>
+          <li><a href="{{route('roles.index')}}">Roles</a></li>
+          <li><a href="{{route('permissions.index')}}">Permissions</a></li>
+        </ul>
+      </li>
     </ul>
   </aside>
 </div>
